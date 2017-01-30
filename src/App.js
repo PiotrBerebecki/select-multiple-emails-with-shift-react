d2ae779253f6161badb55e06e8e4b3319a1dddaf
PiotrBerebecki <petebere@gmail.com>
2017-01-30 11:20:07 +0000

    Fetch list of users with axios

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
 import React, { Component } from 'react';
-import './App.css';
+import axios from 'axios';
 import Item from './Item';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      messages: [
-        'This is an inbox layout',
-        'Check one item',
-        'Hold down your Shift key',
-        'Check a lower item',
-        'Everything in-between should also be set to checked / unchecked'
-      ]
+      messages: null, 
     };
   }
   
-  render() {
+  componentDidMount() {
+    const messages = [
+      'This is an inbox layout',
+      'Check one item',
+      'Hold down your Shift key',
+      'Check a lower item',
+      'Everything in-between should also be set to checked / unchecked'
+    ];
     
-    const renderItems = this.state.messages.map((message, index) => {
-      return <Item key={index} text={message}/>;
-    });
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        const users = response.data.map(user => user.email);
+        this.setState({
+          messages: [
+            ...messages,
+            ...users
+          ]
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }  
+  
+  render() {
+    let renderItems = null;
+    const { messages } = this.state;
+    
+    if (messages) {
+      renderItems = messages.map((message, index) => {
+        return <Item key={index} text={message}/>;
+      });
+    }
     
     return (
       
       <div className="inbox">
-          
           { renderItems }
-          
       </div>
     
     );
